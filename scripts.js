111b557160fe74f9fdb06e05674db05ef34b1047
mc2398 <148909533+mc2398@users.noreply.github.com>
2023-12-08 02:30:37 -0500

    working on resolving navbar sitch

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("'#dropdown").onmouseover=function(use){
+    document.getElementById("usability-dropdown").style.visibility = "visible";
+};
+
+document.getElementById("'#dropdown").onmouseout=function(nouse){
+    document.getElementById("usability-dropdown").style.visibility = "hidden";
+};
